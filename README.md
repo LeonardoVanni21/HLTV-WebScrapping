148bd963ee68d679a25530c79c4ed674b8f2f778
Leonardo Vanni Bonavigo <leonardovanni21@gmail.com>
2024-03-25 21:05:46 -0300

    Updating things

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # HLTV-WebScrapping
- 
+
+## Descrição
+
+Esse é um projeto de web scrapping que raspa o site da HLTV para os últimos torneios de CS2 e seus detalhes, e também o ranking de players de toda a história do CS com pelo menos 50 mapas jogados entre times top 30 da HLTV. O projeto é escrito em Python e usa a biblioteca BeautifulSoup para raspar o site.
+Foi feito para a disciplina de Desenvolvimento de Sistemas Distribuídos da Unisenai de Chapecó/SC.
+
+## Instalação
+
+Para instalar as dependências do projeto, execute o seguinte comando:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Uso
+
+Para rodar o projeto, apenas execute o arquivo `main.py`, e informe o que deseja acessar rank ou torneio e em caso de torneio informe a url do mesmo.
+
+```bash
+python main.py
+```
+
+Espere o programa terminar de rodar e verifique o arquivo `data.csv` para ver os resultados.
+
+## Feito por
+
+- [Leonardo Vanni Bonavigo]
--- main.py
+++ main.py
@@ -2,15 +2,6 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import pandas as pd
 
-Team = {
-    "name": "",
-    "players": [],
-    "roundsWon": 0,
-    "roundsLost": 0,
-    "result": "",
-    "matches": []
-}
-
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -20,44 +11,71 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
 }
 
-url = input("Insira a URL do torneio na HLTV (ex:https://www.hltv.org/events/7148/pgl-cs2-major-copenhagen-2024): ")
-req = Request(url, headers=headers)
-html = urlopen(req)
-soup = BeautifulSoup(html, "html.parser")
-table = soup.findAll(id="GroupPlay")
-teams = table[0].findAll("table", {"class": "standard-box"})[0].findAll("tr")
-teams.pop(0)
-
 data = []
 
-for team in teams:
-    players_url = "https://www.hltv.org/" + team.find("div", {"class": "text-ellipsis"}).find("a")["href"]
-    req = Request(players_url, headers=headers)
+accessRanksOrTournaments = input("Você deseja acessar o rank de players ou algum torneio? (rank/torneio):")
+if accessRanksOrTournaments == "rank":
+    url = "https://www.hltv.org/stats/players?startDate=all&rankingFilter=Top30&minMapCount=50"
+    req = Request(url, headers=headers)
+    print("Requesting: " + url)
     html = urlopen(req)
     soup = BeautifulSoup(html, "html.parser")
-    players = soup.findAll("div", {"class": "playerFlagName"})
-    players = [player.find("span", {"class": "text-ellipsis"}).text for player in players]
-
-    team_data = {
-        "name": team.find("div", {"class": "text-ellipsis"}).text.replace("\n", ""),
-        "players": players,
-        "roundsWon": int(team.find("div", {"class": "cell-width-rw"}).text),
-        "roundsLost": int(team.find("div", {"class": "cell-width-rl"}).text),
-        "result": team.find("div", {"class": "cell-width-record"}).text,
-        "matches": []
-    }
-    matches = team.findAll("a", {"class": "bottom-row"})
-    for match in matches:
-        team1 = match.findAll("span", {"class": "text-ellipsis"})[0].text
-        team2 = match.findAll("span", {"class": "text-ellipsis"})[1].text
-        resultLeft = match.find("div", {"class": "score-left"}).text
-        resultRight = match.find("div", {"class": "score-right"}).text
-        match_data = {
-            "result": f"{team1} {resultLeft} - {resultRight} {team2}",
+    players = soup.find("table", {"class": "stats-table player-ratings-table"}).find("tbody").findAll("tr")
+    for player in players:
+        player_data = {
+            "country": player.find("img", {"class": "flag"})["title"],
+            "name": player.find("td", {"class": "playerCol"}).find("a").text,
+            "teams": player.find("td", {"class": "teamCol"}).find("a").find("img")["title"],
+            "maps": int(player.findAll("td", {"class": "statsDetail"})[0].text),
+            "rounds": int(player.findAll("td", {"class": "statsDetail"})[1].text),
+            "K/D Diff": int(player.find("td", {"class": "kdDiffCol"}).text),
+            "K/D": float(player.findAll("td", {"class": "statsDetail"})[2].text),
+            "Rating 1.0": float(player.find("td", {"class": "ratingCol"}).text),
         }
-        team_data["matches"].append(match_data)
-    data.append(team_data)
+        data.append(player_data)
+else:
+    url = input("Insira a URL do torneio na HLTV (ex:https://www.hltv.org/events/7148/pgl-cs2-major-copenhagen-2024):")
+    req = Request(url, headers=headers)
+    print("Requesting: " + url)
+    html = urlopen(req)
+    soup = BeautifulSoup(html, "html.parser")
+    tournament = soup.find("h1", {"class": "event-hub-title"}).text
+    table = soup.findAll(id="GroupPlay")
+    teams = table[0].findAll("table", {"class": "standard-box"})[0].findAll("tr")
+    teams.pop(0)
+
+    for team in teams:
+        players_url = "https://www.hltv.org/" + team.find("div", {"class": "text-ellipsis"}).find("a")["href"]
+        req = Request(players_url, headers=headers)
+        print("Requesting: " + players_url)
+        html = urlopen(req)
+        soup = BeautifulSoup(html, "html.parser")
+        players = soup.findAll("div", {"class": "playerFlagName"})
+        players = [player.find("span", {"class": "text-ellipsis"}).text for player in players]
+
+        team_data = {
+            "name": team.find("div", {"class": "text-ellipsis"}).text.replace("\n", ""),
+            "tournament": tournament,
+            "players": players,
+            "roundsWon": int(team.find("div", {"class": "cell-width-rw"}).text),
+            "roundsLost": int(team.find("div", {"class": "cell-width-rl"}).text),
+            "result": team.find("div", {"class": "cell-width-record"}).text,
+            "matches": []
+        }
+        matches = team.findAll("a", {"class": "bottom-row"})
+        for match in matches:
+            team1 = match.findAll("span", {"class": "text-ellipsis"})[0].text
+            team2 = match.findAll("span", {"class": "text-ellipsis"})[1].text
+            resultLeft = match.find("div", {"class": "score-left"}).text
+            resultRight = match.find("div", {"class": "score-right"}).text
+            match_data = {
+                "result": f"{team1} {resultLeft} - {resultRight} {team2}",
+            }
+            team_data["matches"].append(match_data)
+        data.append(team_data)
 
 df = pd.DataFrame(data)
+
+df.to_csv("data.csv", index=False)
 
 print(df.to_string(index=False))
